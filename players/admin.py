0465acc5c893b38ce526959ed58fb2af64075589
Victor <victorliang@hotmail.com>
2023-04-07 15:40:30 -0400

    custom admin pages

--- players/admin.py
+++ players/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Player, Club
 
-# Register your models here.
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    pass
