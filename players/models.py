03f51becc17b5f010559d785141822f32c22e067
Victor <victorliang@hotmail.com>
2023-04-07 14:12:10 -0400

    create first models

--- players/models.py
+++ players/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Club(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class Player(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    club = models.ManyToManyField(Club)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
