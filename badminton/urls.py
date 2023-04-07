b4c9fdb78b22699b9fbcddf3ca68fe4c44618b13
Victor <victorliang@hotmail.com>
2023-04-07 13:29:14 -0400

    create players app

--- badminton/urls.py
+++ badminton/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('players', include('players.urls')),
 ]
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='players/', permanent=True)),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
